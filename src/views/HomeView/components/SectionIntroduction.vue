7e9e28904d7857064f0791c05b18643652e6694e
gawr-fiude <eduardo.e.goto@gmail.com>
2024-11-30 23:25:09 -0300

    chore: add picture to introduction

--- src/views/HomeView/components/SectionIntroduction.vue
+++ src/views/HomeView/components/SectionIntroduction.vue
@@ -1,43 +1,47 @@
 <template>
   <section class="home-section">
-    <h1>
-      Hello World! <br />
-      I'm
-      <span class="name"
-        >Eduardo Eiji
+    <img src="/me.webp" alt="Picture of myself." class="me-picture" />
+
+    <article>
+      <h1>
+        Hello World! <br />
+        I'm
+        <span class="name"
+          >Eduardo Eiji
+          <a
+            href="https://en.wikipedia.org/wiki/Goto"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Goto</a
+          ></span
+        >
+      </h1>
+
+      <p>
+        Just a chill Developer from
         <a
-          href="https://en.wikipedia.org/wiki/Goto"
+          href="https://en.wikipedia.org/wiki/Curitiba"
           target="_blank"
           rel="noopener noreferrer"
-          >Goto</a
-        ></span
-      >
-    </h1>
+          >Curitiba</a
+        >. <br />
+        Trying to make the world a better place, one
+        <span class="line-through">div</span> element at a time.
+      </p>
 
-    <p>
-      Just a chill Developer from
-      <a
-        href="https://en.wikipedia.org/wiki/Curitiba"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Curitiba</a
-      >. <br />
-      Trying to make the world a better place, one
-      <span class="line-through">div</span> element at a time.
-    </p>
-
-    <div class="button-group">
-      <a
-        v-for="link in socialLinks"
-        :key="link.label + link.to"
-        :href="link.to"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="social-link"
-      >
-        <span>{{ link.label }}</span>
-      </a>
-    </div>
+      <div class="button-group">
+        <a
+          v-for="link in socialLinks"
+          :key="link.label + link.to"
+          :href="link.to"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="social-link"
+        >
+          <span>{{ link.label }}</span>
+        </a>
+      </div>
+    </article>
   </section>
 </template>
 
@@ -57,6 +61,9 @@ const socialLinks = [
 <style scoped>
 section {
   margin-top: 8rem;
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: auto auto;
 }
 
 .name {
@@ -89,5 +96,12 @@ p {
   padding: 0.5rem 1rem;
   border: 2px dashed var(--color-primary);
   text-decoration: none;
+}
+
+.me-picture {
+  width: 18rem;
+  aspect-ratio: 1;
+  object-fit: cover;
+  border-radius: 100%;
 }
 </style>
